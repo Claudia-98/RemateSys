65090de850355ae4123eeb9f04a0c56b1f391479
unknown <jru981@gmail.com>
2020-10-21 18:29:26 -0600

    Agregando unas migraciones

---
+++ database/migrations/2020_10_22_001133_create_prodcuto_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProdcutoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prodcuto', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre', 100);
+            $table->string('alias', 50)->nullable();
+            $table->string('descripcion', 100)->nullable();
+            $table->double('precio_venta');
+            $table->double('precio_mayorista')->nullable();
+            $table->double('precio_compra')->nullable();
+            // $table->double('notify')->nullable();
+
+            $table->integer('idtipo')->unsigned();
+            $table->foreign('idtipo')->references('id')->on('tipoproducto');
+
+            $table->integer('idmedida')->unsigned();
+            $table->foreign('idmedida')->references('id')->on('medidaproducto');
+            
+            $table->boolean('estado')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prodcuto');
+    }
+}
--- database/migrations/2020_10_22_002442_create_users_table.php
+++ database/migrations/2020_10_22_002442_create_users_table.php
@@ -14,13 +14,18 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->integer('id')->unsigned();
+            $table->foreign('id')->references('id')->on('personas')->onDelete('cascade');
+
+            $table->integer('idrol')->unsigned();
+            $table->foreign('idrol')->references('id')->on('roles')->onDelete('cascade');
+            
+            $table->string('usuario', 45)->unique();
             $table->string('password');
+            $table->string('foto');
+            $table->boolean('estado')->default(1);
+
             $table->rememberToken();
-            $table->timestamps();
         });
     }
 
