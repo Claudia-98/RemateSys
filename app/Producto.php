902a3399f8ae94b688001e829d860e748c00cf6d
rosh981pc <jru981@gmail.com>
2020-11-08 06:28:44 -0800

    Dejando al dia para transicion de disco

--- app/Producto.php
+++ app/Producto.php
@@ -9,5 +9,14 @@ class Producto extends Model
     protected $table = 'producto';
     protected $fillable = ['nombre','alias','codigo','descripcion','foto','precio_venta','precio_mayorista',
     'precio_compra','idpersonas','idtipo','idmedida','estado'];
-    
+
+    public function proveedor(){
+        return $this->hasMany('App\Persona','idpersonas','id');
+    }
+    public function tipo(){
+        return $this->hasMany('App\Categoria','idtipo','id');
+    }
+    public function medida(){
+        return $this->hasMany('App\MedidaProducto','idmedida','id');
+    }
 }
